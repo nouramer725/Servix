605c2a8e41586ed85ecbca89121849b8f4a817be
Nour <nouramer725@gmail.com>
2025-03-20 19:52:31 +0200

    add easy_localization to Privacy Policy screen

--- lib/Settings/Privacy Policy.dart
+++ lib/Settings/Privacy Policy.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:easy_localization/easy_localization.dart'; // Import easy_localization
 
 class PrivacyPolicy extends StatelessWidget {
   const PrivacyPolicy({super.key});
@@ -11,12 +12,14 @@ class PrivacyPolicy extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: Text("Privacy Policy",
-            style: GoogleFonts.cantataOne(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            )),
+        title: Text(
+          "Privacy Policy".tr(),
+          style: GoogleFonts.cantataOne(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
       ),
       backgroundColor: Colors.white,
       body: Padding(
@@ -36,99 +39,118 @@ class PrivacyPolicy extends StatelessWidget {
                 ),
                 Container(
                   decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.30),
-                          spreadRadius: 0.5,
-                          blurRadius: 5,
-                          offset: Offset(5, 5),
-                        )
-                      ]),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.30),
+                        spreadRadius: 0.5,
+                        blurRadius: 5,
+                        offset: const Offset(5, 5),
+                      )
+                    ],
+                  ),
                   child: Padding(
                     padding: const EdgeInsets.all(16.0),
                     child: RichText(
                       text: TextSpan(
                         style: GoogleFonts.castoro(
-                            fontSize: 12, color: Colors.black),
+                          fontSize: 12,
+                          color: Colors.black,
+                        ),
                         children: [
                           TextSpan(
-                            text: 'The SERVIX ',
+                            text: 'The SERVIX '.tr(),
                             style: GoogleFonts.calistoga(
-                                fontWeight: FontWeight.bold),
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                           TextSpan(
                             text:
-                                ' application is considered a link between customers and service providers without any legal, physical or financial responsibility.\n',
+                                ' application is considered a link between customers and service providers without any legal, physical or financial responsibility.\n'
+                                    .tr(),
                           ),
                           TextSpan(
-                            text: 'We are not responsible ',
+                            text: 'We are not responsible '.tr(),
                           ),
                           TextSpan(
                             text:
-                                'for the quality of service provided by service providers to customers required using the ',
+                                'for the quality of service provided by service providers to customers required using the '
+                                    .tr(),
                           ),
                           TextSpan(
-                            text: 'SERVIX ',
+                            text: 'SERVIX '.tr(),
                             style: GoogleFonts.calistoga(
-                                fontWeight: FontWeight.bold),
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                           TextSpan(
                             text:
-                                ' application or the service and do not bear responsibility in this aspect, provided that the ',
+                                ' application or the service and do not bear responsibility in this aspect, provided that the '
+                                    .tr(),
                           ),
                           TextSpan(
-                            text: 'SERVIX ',
+                            text: 'SERVIX '.tr(),
                             style: GoogleFonts.calistoga(
-                                fontWeight: FontWeight.bold),
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                           TextSpan(
-                            text: 'has the right to review this.\n',
+                            text: 'has the right to review this.\n'.tr(),
                           ),
                           TextSpan(
-                            text: 'The SERVIX ',
+                            text: 'The SERVIX '.tr(),
                             style: GoogleFonts.calistoga(
-                                fontWeight: FontWeight.bold),
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                           TextSpan(
                             text:
-                                'is not responsible for any acts, actions, behavior or negligence, or all of the above, on the part of the service providers.\n',
+                                'is not responsible for any acts, actions, behavior or negligence, or all of the above, on the part of the service providers.\n'
+                                    .tr(),
                           ),
                           TextSpan(
                             text:
-                                'Any complaints about service providers submitted by customers should be submitted to the ',
+                                'Any complaints about service providers submitted by customers should be submitted to the '
+                                    .tr(),
                           ),
                           TextSpan(
-                            text: 'SERVIX ',
+                            text: 'SERVIX '.tr(),
                             style: GoogleFonts.calistoga(
-                                fontWeight: FontWeight.bold),
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                           TextSpan(
                             text:
-                                'directly, provided that it does not bear any responsibility in this aspect, and is only a link between the two parties.\n',
+                                'directly, provided that it does not bear any responsibility in this aspect, and is only a link between the two parties.\n'
+                                    .tr(),
                           ),
                           TextSpan(
-                            text: 'SERVIX ',
+                            text: 'SERVIX '.tr(),
                             style: GoogleFonts.calistoga(
-                                fontWeight: FontWeight.bold),
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                           TextSpan(
                             text:
-                                'has the right to provide any information to official authorities if requested to do so.\n',
+                                'has the right to provide any information to official authorities if requested to do so.\n'
+                                    .tr(),
                           ),
                           TextSpan(
-                            text: 'The SERVIX ',
+                            text: 'The SERVIX '.tr(),
                             style: GoogleFonts.calistoga(
-                                fontWeight: FontWeight.bold),
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                           TextSpan(
                             text:
-                                ' application does not take any commission or sum of money from the service requester in any way.\n',
+                                ' application does not take any commission or sum of money from the service requester in any way.\n'
+                                    .tr(),
                           ),
                           TextSpan(
                             text:
-                                'Fees for obtaining applications are paid by service providers only, through bank transfer only or charging a balance.\n',
+                                'Fees for obtaining applications are paid by service providers only, through bank transfer only or charging a balance.\n'
+                                    .tr(),
                           ),
                         ],
                       ),
